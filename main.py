edb0d3ec7027de639960026d6bcd612e3466a294
Yash Goswami <yashgoswami148@gmail.com>
2021-11-26 13:54:39 +0530

    complete post request

---
+++ main.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+class Post(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int] = None
+    published: bool = True
+
+my_posts = [
+    {"title":"first post","content":"This is the first post","id":1},
+    {"title":"second post","content":"This is the second post","id":2}
+    ]
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"This is a test"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"posts":my_posts}
+
+@app.get("/posts/{id}")
+async def get_post(id:int,response:Response):
+    post = find_post(id)
+    if post == None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message":"No post found"}
+    return {"post":post}
+
+@app.post("/posts")
+async def create_posts(post: Post):
+    p = post.dict()
+    p["id"] = randrange(0,100000)
+    my_posts.append(p)
+    return {"post":p}
