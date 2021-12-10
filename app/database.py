22eb9eb6a4b6d671fd03b4064e533a4fce10dcd8
Yash Goswami <yashgoswami148@gmail.com>
2021-12-10 18:36:36 +0530

    used env variables

--- app/database.py
+++ app/database.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:yash1234@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -20,12 +21,12 @@ def get_db():
     finally:
         db.close()
 
-while True:
-    try:
-        connection = psycopg2.connect(host="localhost",dbname="fastapi",user="postgres",password="yash1234",cursor_factory=RealDictCursor)
-        cursor = connection.cursor()
-        print("Connection successful!")
-        break
-    except Exception:
-        print("Connection unsuccessful!")
-        time.sleep(30)
+# while True:
+#     try:
+#         connection = psycopg2.connect(host="localhost",dbname="fastapi",user="postgres",password="yash1234",cursor_factory=RealDictCursor)
+#         cursor = connection.cursor()
+#         print("Connection successful!")
+#         break
+#     except Exception:
+#         print("Connection unsuccessful!")
+#         time.sleep(30)
